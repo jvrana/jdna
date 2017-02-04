bed736595494971a245f596c0de30d4f4e4416e3
jvrana <jvrana@uw.edu>
2017-02-04 12:12:52 -0800

    alignment_viewer

---
+++ DAS/alignment_viewer/baseviewer.coffee
@@ -0,0 +1,123 @@
+w = 960
+h = 500
+
+svg = d3.select('body').append('svg')
+  .attr('width', w)
+  .attr('height', h)
+
+#jsonCircles = [
+#  { "x_axis": 30, "y_axis": 30, "radius": 20, "color" : "green" },
+#  { "x_axis": 70, "y_axis": 70, "radius": 20, "color" : "purple"},
+#  { "x_axis": 110, "y_axis": 100, "radius": 20, "color" : "red"}
+#]
+#
+#circles = svg.selectAll("circles")
+#  .data(jsonCircles)
+#  .enter()
+#  .append("circle")
+#  .attr("cx", (d) ->  d.x_axis )
+#  .attr("cy", (d) ->  d.y_axis )
+#  .attr("r", (d) ->  d.radius )
+#  .style("fill", (d) ->  d.color )
+
+xpadding = 50.0
+
+
+tooltip = d3.select("body")
+.append("div")
+.style("position", "absolute")
+.style("z-index", "10")
+.style("visibility", "hidden")
+.text("a simple tooltip")
+
+
+
+d3.json('data.json', (data) ->
+
+  query_len = data.alignments[0].query_length
+
+  xScale = d3.scaleLinear()
+    .domain([0, query_len])
+    .range([0+xpadding, w-xpadding])
+
+  yScale = d3.scaleLinear()
+    .domain([0, data.alignments.length])
+    .range([120, 500])
+
+  svg.append('rect')
+    .attr('x', xScale(0))
+    .attr('width', xScale(query_len))
+    .attr('y', 100)
+    .attr('height', 10)
+    .attr('opacity', 0.5)
+    .attr('fill', 'blue')
+
+  svg.selectAll('rects')
+    .data(data.alignments)
+    .enter()
+    .append('rect')
+    .attr('x', (d) -> xScale(d.q_start))
+    .attr('y', (d,i) -> 120 + i * 10)
+    .attr('width', (d) -> xScale(d.q_end) - xScale(d.q_start))
+    .attr('height', 7)
+    .attr('fill', 'black')
+    .attr('opacity', 0.9)
+    .on("mouseover", (d) ->
+      tooltip.text(d.subject_acc + ' ' + d.q_start + ' (' + xScale(d.q_start) + ') ' + d.q_end + ' (' + xScale(d.q_end) + ') ')
+      tooltip.style("visibility", "visible")
+    )
+    .on("mouseout", () -> tooltip.style("visibility", "hidden") )
+)
+
+
+
+#width = 200
+#tspan = text.text(null).append('tspan')
+#tspan.text(txt)
+#if tspan.node().getComputedTextLength() > width)
+#alert(tspan.node().getComputedTextLength())
+#fill = d3.scale.category10()
+#nodes = d3.range(100).map(Object)
+#
+#vis = d3.select("#chart").append("svg:svg")
+#.attr("width", w)
+#.attr("height", h)
+#
+#force = d3.layout.force()
+#.nodes(nodes)
+#.links([])
+#.size([w, h])
+#.start()
+#
+#node = vis.selectAll("circle.node")
+#.data(nodes)
+#.enter().append("svg:circle")
+#.attr("class", "node")
+#.attr("cx", (d) -> d.x)
+#.attr("cy", (d) -> return d.y)
+#.attr("r", 8)
+#.style("fill", (d, i) -> fill(i & 3) )
+#.style("stroke", (d, i) -> d3.rgb(fill(i & 3)).darker(2) )
+#.style("stroke-width", 1.5)
+#.call(force.drag)
+#
+#vis.style("opacity", 1e-6)
+#.transition()
+#.duration(1000)
+#.style("opacity", 1)
+#
+#force.on "tick", (e) ->
+## Push different nodes in different directions for clustering.
+#  k = 6 * e.alpha
+#  nodes.forEach (o, i) ->
+#    o.x += if i & 2 then k else -k
+#    o.y += if i & 1 then k else -k
+#  node.attr("cx", (d) -> d.x )
+#  .attr("cy", (d) -> d.y )
+#
+#
+#d3.select("body").on "click", () ->
+#  nodes.forEach (o, i) ->
+#    o.x += (Math.random() - 0.5) * 40
+#    o.y += (Math.random() - 0.5) * 40
+#  force.resume()
--- DAS/blast.py
+++ DAS/blast.py
@@ -3,6 +3,8 @@ import subprocess
 import shlex
 from Bio import SeqIO
 import os
+import re
+import json
 
 locations = dict(
     database="database",
@@ -19,26 +21,30 @@ def run_cmd(cmd_str, **kwargs):
     args = shlex.split(cmd_str.format(**kwargs))
     output = subprocess.Popen(args)
 
+def open_sequence(filename, fullname=True, **fmt):
+    formats = {"gb": "genbank", "fsa": "fasta", "fasta": "fasta"}
+    formats.update(fmt)
+    prefix, suffix = os.path.basename(filename).split('.')
+    seqs = []
+    with open(filename, 'rU') as output_handle:
+        s = list(SeqIO.parse(output_handle, formats[suffix]))
+        # Rename accession id to full name
+        if len(s) == 1 and fullname:
+            s[0].id = prefix
+        seqs += s
+    return seqs
+
 def concat_seqs(idir, odir, out_name):
     sequences = []
 
-    formats = {"genbank": ["gb"], "fasta": ["fsa", "fasta"]}
-
-    for fmt in formats:
-        filenames = []
-        for suffix in formats[fmt]:
-            paths = os.path.join(idir, '*.{}'.format(suffix))
-            filenames += glob(paths)
-
-        for filename in filenames:
-            with open(filename, 'rU') as input_handle:
-                sequences += SeqIO.parse(input_handle, fmt)
-
+    filenames = glob(os.path.join(idir, '*.*'))
+    for filename in filenames:
+        print 'opening', filename
+        sequences += open_sequence(filename)
     output_path = os.path.join(odir, out_name + '.fsa')
     with open(output_path, "w") as output_handle:
         SeqIO.write(sequences, output_handle, "fasta")
     return output_path
-
 
 def makedb(input, title, output):
     sequences = []
@@ -55,32 +61,64 @@ def makedb(input, title, output):
     return output
 
 def gb_to_fsa(input, output):
-    sequences = []
-    with open(input, 'rU') as input_handle, open(output, 'w') as output_handle:
-        sequences += SeqIO.parse(input_handle, 'genbank')
+    sequences = open_sequence(input)
+    with open(output, 'w') as output_handle:
         SeqIO.write(sequences, output_handle, 'fasta')
     return output
 
-def runblast(db, query, out, outfmt=10):
-    "blastn -db db.fsa -query 9320_\ pLSL-[ADH1]-p35S\:mTURQ\:t35S-[ADH1]-R.fsa -out results.out"
+def runblast(db, query, out, output_format=7, output_views="qacc sacc score evalue bitscore\
+ length nident gapopen gaps qlen qstart qend slen sstart send sstrand qseq sseq"):
 
     prefix, suffix = query.split('.')
     if suffix == 'gb':
         query = gb_to_fsa(query, prefix + '.fsa')
-
+    outfmt = '\"{} {}\"'.format(output_format, output_views)
     cmd_str = "blastn -db {db} -query {query} -out {out} -outfmt {outfmt}"
     run_cmd(cmd_str, db=db, query=query, out=out, outfmt=outfmt)
-    return out
+
+    results = ''
+    with open(out, 'rU') as output_handle:
+        results = output_handle.read()
+
+    return results
+# Query: pMODKan-HO-pACT1-out
 
 c = concat_seqs(locations['templates'], locations['database'], 'db')
 db = makedb(c, 'db', os.path.join(locations['database'], 'db'))
+# query acc., subject acc., % identity, alignment length, mismatches, gap opens, q. start, q. end, s. start, s. end, evalue, bit score
 
 
+def parse_results(results):
+    g = re.search('# (?P<blast_ver>.+)\n# Query: (?P<query>.+)\n# Database: (?P<database>.+)\n# Fields: (?P<fields>.+)', results)
+    print g
+    print results
+    meta = g.groupdict()
 
-runblast(db,
-         os.path.join(locations['designs'], 'pmodkan-ho-pact1-z4-er-vpr.gb'),
-         os.path.join(locations['database'], 'results.out')
-         )
+    matches = re.findall('\n([^#].*)', results)
+    meta['fields'] = re.split('\s*,\s*', meta['fields'])
+    # clean up fields
+    for i, f in enumerate(meta['fields']):
+        meta['fields'][i] = f.replace('.', '')\
+            .replace(' ', '_')\
+            .replace('%', 'perc')
+    alignments = []
+    for m in matches:
+        alignments.append(
+            dict(zip(meta['fields'], m.split('\t')))
+        )
+
+    return {'meta': meta, 'alignments': alignments}
+
+# Todo: Double all dnas to account for cyclic plasmids (if cyclic)
+# TODO: if linearized, can use directly
+# TODO: find all primer positions
+
+r = runblast(db,
+         os.path.join(locations['designs'], 'pmodkan-ho-pact1-z4-er-vpr_doubled_temp.gb'),
+         os.path.join(locations['database'], 'results.out'))
+p = parse_results(r)
+with open('alignment_viewer/data.json', 'w') as output_handle:
+    json.dump(p, output_handle)
 # def blastn_cmd(db='', query='', out=''):
 #     cmd_str = "blastn -db {database} -query {query} -out {out}".format(database=db, query=query, out=out)
 #     args = shlex.strip(cmd_str)
