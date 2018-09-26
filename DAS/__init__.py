7b5789ca53ddbe80ebe1e6519ff55323fa234c09
jvrana <justin.vrana@gmail.com>
2018-09-26 10:52:41 -0700

    remove DAS

--- DAS/__init__.py
+++
@@ -1,9 +0,0 @@
-'''
-Project: jdna
-File: __init__.py
-Author: Justin
-Date: 2/6/17
-
-Description: 
-
-'''
--- DAS/das_multithreading.py
+++
@@ -1,9 +0,0 @@
-'''
-Project: jdna
-File: das_multithreading
-Author: Justin
-Date: 2/13/17
-
-Description: 
-
-'''
