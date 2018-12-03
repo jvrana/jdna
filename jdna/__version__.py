e82ec38c43058d06ad2320371214f2afa1d4e33f
jvrana <justin.vrana@gmail.com>
2018-12-02 21:24:33 -0800

    docks and bump

--- jdna/__version__.py
+++ jdna/__version__.py
@@ -4,6 +4,6 @@ __title__ = 'jdna'
 __description__ = 'a dna sequence editor'
 __url__ = ''
 __pypi__ = ''
-__version__ = '0.0.3'
+__version__ = '0.0.4'
 __author__ = 'Justin D Vrana'
 __author_email__ = "justin.vrana@gmail.com"
